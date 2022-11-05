251f732383606bdd421645247faa3c9cd215339a
KRShee <KRShee@gmail.com>
2022-11-04 21:50:52 -0500

    Almost done- Need to display user choices Done

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+
+    <h1>Would you rather...</h1>
+
+    <WouldYouRather v-bind:question="wyrQuestion"
+    v-bind:answer1="wyrAnswer1"
+    v-bind:answer2="wyrAnswer2"
+    v-on:answer-changed="answerChanged"></WouldYouRather>
+
+    <p>{{userSelectionMessage}}</p>
+
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    WouldYouRather
+  },
+  data(){
+    return{
+      wyrQuestion: 'Would you rather live in a house shaped like a triangle or a house shaped like a circle?',
+      wyrAnswer1: 'Triangle house',
+      wyrAnswer2: 'Circle house',
+      userSelectionMessage: ''
+
+    }
+    },
+    methods:{
+      answerChanged(choice){
+        this.userSelectionMessage = `Thanks! you chose ${choice}`
+
+      }
+    }
 }
 </script>
 
 <style>
+
+body{
+  background: violet;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +52,12 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  padding: 10px;
+  margin-top: 60px;
+  background: bluebiolet;
+}
+
+p{
+  font-style: italic;
 }
 </style>
